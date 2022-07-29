0a6bb9e338bddbb8898fd669a192c14a096e0b56
Alexandr <sanier20@mail.ru>
2022-07-29 12:13:00 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SemiSync
+{
+    public class Program
+    {
+        private static async Task HandleAsync()
+        {
+            Console.WriteLine("Run");
+            await Task.Delay(1000);
+            Console.WriteLine("Exit");
+        }
+        static async Task Main()
+        {
+            Task t = HandleAsync();
+            if (await Task.WhenAny(t, Task.Delay(5000)) == t)
+            {
+                Console.WriteLine("It`s Ok");
+            }
+            else
+            {
+                Console.WriteLine("Bad. Time Out");
+            }
+            Console.WriteLine("Done");
+        }
+    }
+}
